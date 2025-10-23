925d36a9475b48c6d14fa2a0289f51072424d09e
ZBetsch <betschzt@mail.uc.edu>
2025-10-23 18:22:58 -0400

    Initial Files

---
+++ src/main/java/com/group5final/roomieradar/RoomieRadarApplication.java
@@ -0,0 +1,13 @@
+package com.group5final.roomieradar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomieRadarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomieRadarApplication.class, args);
+    }
+
+}
